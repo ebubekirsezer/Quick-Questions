c11e229244a694b264fbe0a896bf909027bf0e92
EbubekirSezer <ebubekirssezer@gmail.com>
2020-09-07 11:27:58 +0300

    Sign Activity created

---
+++ QuickQuestions/app/src/main/java/com/ess/quickquestions/SignActivity.kt
@@ -0,0 +1,11 @@
+package com.ess.quickquestions
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign)
+    }
+}
