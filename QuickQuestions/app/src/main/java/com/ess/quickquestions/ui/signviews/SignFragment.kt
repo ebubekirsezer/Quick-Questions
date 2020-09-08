3c23977026b3b146e1e7d2caf9e7641e11e9885a
EbubekirSezer <ebubekirssezer@gmail.com>
2020-09-08 11:11:58 +0300

    sign tab colors arranged

--- QuickQuestions/app/src/main/java/com/ess/quickquestions/ui/signviews/SignFragment.kt
+++ QuickQuestions/app/src/main/java/com/ess/quickquestions/ui/signviews/SignFragment.kt
@@ -1,11 +1,14 @@
 package com.ess.quickquestions.ui.signviews
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.ess.quickquestions.R
+import kotlinx.android.synthetic.main.fragment_sign.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -30,12 +33,32 @@ class SignFragment : Fragment() {
         }
     }
 
+    @SuppressLint("ResourceType")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_sign, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        button_sign_in.setOnClickListener {
+            button_sign_in.setTextColor(resources.getColor(R.color.textColorPrimary))
+            horizontal_sign_in_line.setBackgroundColor(resources.getColor(R.color.textColorPrimary))
+            button_sign_up.setTextColor(resources.getColor(R.color.textGreyColor))
+            horizontal_sign_up_line.setBackgroundColor(resources.getColor(R.color.textGreyColor))
+        }
+
+        button_sign_up.setOnClickListener {
+            button_sign_in.setTextColor(resources.getColor(R.color.textGreyColor))
+            horizontal_sign_in_line.setBackgroundColor(resources.getColor(R.color.textGreyColor))
+            button_sign_up.setTextColor(resources.getColor(R.color.textColorPrimary))
+            horizontal_sign_up_line.setBackgroundColor(resources.getColor(R.color.textColorPrimary))
+        }
+
+        super.onViewCreated(view, savedInstanceState)
     }
 
     companion object {
