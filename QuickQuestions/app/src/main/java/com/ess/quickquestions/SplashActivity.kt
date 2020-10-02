e45ebf1ec61bf49be4cd5079619475115f06dded
EbubekirSezer <ebubekirssezer@gmail.com>
2020-10-02 09:50:31 +0300

    splash added

---
+++ QuickQuestions/app/src/main/java/com/ess/quickquestions/SplashActivity.kt
@@ -0,0 +1,14 @@
+package com.ess.quickquestions
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        startActivity(Intent(applicationContext, SignActivity::class.java))
+        finish()
+    }
+}
