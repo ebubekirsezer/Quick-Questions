dbe199c3c4f752c61cd3b4ff6b8776727276ab74
EbubekirSezer <ebubekirssezer@gmail.com>
2020-10-03 11:55:27 +0300

    Auth control problem solved nav graph start destination changed

--- QuickQuestions/app/src/main/java/com/ess/quickquestions/MainActivity.kt
+++ QuickQuestions/app/src/main/java/com/ess/quickquestions/MainActivity.kt
@@ -2,10 +2,31 @@ package com.ess.quickquestions
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.NavGraph
+import androidx.navigation.fragment.NavHostFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var navGraph: NavGraph
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val isAuth = intent.getBooleanExtra("isAuth",false)
+        println(isAuth)
+
+        val navHostFragment = sign_fragment as NavHostFragment
+        val graphInflater = navHostFragment.navController.navInflater
+        navGraph = graphInflater.inflate(R.navigation.main_navigation)
+        navController = navHostFragment.navController
+
+        val destination = if(isAuth) R.id.homeFragment else R.id.signFragment
+        navGraph.startDestination = destination
+        navController.graph = navGraph
+
     }
 }
--- QuickQuestions/app/src/main/java/com/ess/quickquestions/ui/homeview/HomeFragment.kt
+++ QuickQuestions/app/src/main/java/com/ess/quickquestions/ui/homeview/HomeFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.ess.quickquestions.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
 
 class HomeFragment : Fragment() {
@@ -15,6 +17,7 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 }
