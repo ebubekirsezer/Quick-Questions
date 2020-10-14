204357c243bbf058dd236f7650075bdd389be52d
EbubekirSezer <ebubekirssezer@gmail.com>
2020-10-14 12:01:46 +0300

    category model fetched in the question and showed on the fragment question

--- QuickQuestions/app/src/main/java/com/ess/quickquestions/ui/questionview/QuestionFragment.kt
+++ QuickQuestions/app/src/main/java/com/ess/quickquestions/ui/questionview/QuestionFragment.kt
@@ -5,56 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.ess.quickquestions.R
+import com.ess.quickquestions.databinding.FragmentQuestionBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [QuestionFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class QuestionFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private lateinit var viewModel: QuestionViewModel
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_question, container, false)
-    }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment QuestionFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            QuestionFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        val binding = FragmentQuestionBinding.inflate(inflater)
+
+        val category= QuestionFragmentArgs.fromBundle(requireArguments()).Category
+        binding.questionText.text  = category.questions[0].question
+
+        viewModel = ViewModelProvider(this).get(QuestionViewModel::class.java)
+
+        binding.viewModel = viewModel
+        return binding.root
     }
 }
