6b3b1984725176298ac51b7d9b01b42aab8db10a
EbubekirSezer <ebubekirssezer@gmail.com>
2020-10-16 15:05:57 +0300

    Question Fragment design and styles arranged and adapter added

--- QuickQuestions/app/src/main/java/com/ess/quickquestions/ui/questionview/QuestionCardsAdapter.kt
+++ QuickQuestions/app/src/main/java/com/ess/quickquestions/ui/questionview/QuestionCardsAdapter.kt
@@ -1,2 +1,41 @@
 package com.ess.quickquestions.ui.questionview
 
+import android.text.Layout
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.ess.quickquestions.R
+import com.ess.quickquestions.model.Question
+import kotlinx.android.synthetic.main.question_card.view.*
+
+class QuestionCardsAdapter(
+    private val questionList: List<Question>,
+    private val onClickListener: OnClickListener
+) : RecyclerView.Adapter<QuestionCardsAdapter.RowHolder>() {
+
+    class RowHolder(view: View) : RecyclerView.ViewHolder(view) {
+        fun bind(model: Question){
+        }
+    }
+
+    class OnClickListener(val clickListener: (question: Question) -> Unit) {
+        fun onClick(question: Question) = clickListener(question)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RowHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.question_card, parent, false)
+
+        return RowHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: RowHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        return questionList.count()
+    }
+
+}
--- QuickQuestions/app/src/main/java/com/ess/quickquestions/ui/questionview/QuestionViewModel.kt
+++ QuickQuestions/app/src/main/java/com/ess/quickquestions/ui/questionview/QuestionViewModel.kt
@@ -1,15 +1,23 @@
 package com.ess.quickquestions.ui.questionview
 
 import android.app.Application
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.ess.quickquestions.model.CategoryX
+import com.ess.quickquestions.model.Question
 
 class QuestionViewModel(
     val category: CategoryX,
     application: Application
 ) : ViewModel(){
 
+    private var _questionList = MutableLiveData<List<Question>>()
+    val questionList: LiveData<List<Question>>
+        get() = _questionList
+
     init {
-        println(category)
+        _questionList.value = category.questions
+        println(category.questions)
     }
 }
