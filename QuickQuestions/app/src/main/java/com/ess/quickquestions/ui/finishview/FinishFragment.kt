bd357b1247195a853724b58dbee0fa03a7f0269b
EbubekirSezer <ebubekirssezer@gmail.com>
2020-10-17 16:18:58 +0300

    Score getting from the question and navigation to finish screen

--- QuickQuestions/app/src/main/java/com/ess/quickquestions/ui/finishview/FinishFragment.kt
+++ QuickQuestions/app/src/main/java/com/ess/quickquestions/ui/finishview/FinishFragment.kt
@@ -5,16 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.ess.quickquestions.R
+import com.ess.quickquestions.databinding.FragmentFinishBinding
 
 class FinishFragment : Fragment() {
+
+    private lateinit var viewModel: FinishViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_finish, container, false)
+
+        (activity as AppCompatActivity).supportActionBar?.title = ""
+
+        val binding = FragmentFinishBinding.inflate(inflater)
+        val score = FinishFragmentArgs.fromBundle(requireArguments()).score
+        println(score)
+
+        viewModel = ViewModelProvider(this).get(FinishViewModel::class.java)
+
+        binding.viewModel = viewModel
+
+        return binding.root
     }
 
 }
